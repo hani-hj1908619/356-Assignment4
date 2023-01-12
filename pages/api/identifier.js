ec1cbc7c2c4b1bd442f4a7801367ba4b84607e35
Hani Jassin <haanijessin@gmail.com>
2023-01-12 20:05:52 +0300

    test

--- pages/api/identifier.js
+++ pages/api/identifier.js
@@ -6,12 +6,24 @@ export default async function handler(req, res) {
     try {
       const indentifier = uuidv4()
 
-      const file = await fs.promises.readFile("/tmp/users.json");
-      const users = JSON.parse(file)
-      users.push(indentifier)
-      fs.writeFileSync("/tmp/users.json", JSON.stringify(users))
+      async function handle() {
+        const file = await fs.promises.open("tmp/users.json", 'r+')
+        const fileContent = await file.readFile()
+        let users = []
+        if (fileContent.length != 0) users = JSON.parse(fileContent)
 
-      res.status(200).json(indentifier)
+        users.push(indentifier)
+        await file.writeFile(JSON.stringify(users))
+        file.close()
+        res.status(200).json(indentifier)
+      }
+
+      try {
+        handle()
+      } catch (e) {
+        fs.open("tmp/users.json", "w", (err, fd) => fs.close(fd));
+        handle()
+      }
     } catch (e) {
       res.status(500).json({ error: e.message });
     }
